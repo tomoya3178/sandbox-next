41a6ae73ae358fa4117b9ee357dbe70c032be43f
tomoya3178 <60164103+tomoya3178@users.noreply.github.com>
2022-09-26 16:50:16 +0900

    test

---
+++ pages/index2.tsx
@@ -0,0 +1,8 @@
+import { Button } from '@chakra-ui/react'
+import { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return <Button colorScheme='blue'>Button</Button>
+}
+
+export default Home
